d5d0b32933652101a38824c677fa524bdf0e1971
James Lyndsay <jdl@workroom-productions.com>
2021-04-01 00:38:37 +0100

    wrote drawFilledTable from drawTable, tested with approval test

--- TicTacToe.py
+++ TicTacToe.py
@@ -21,6 +21,17 @@ B |   |   |
 b""")
 	assert (drawFilledRow("A", 2, ["X", "O"]) =="A | X | O |")
 	
+	filledTable = """    1   2   3
+   --- --- ---
+A | X | O |   |
+   --- --- ---
+B | X | X | O |
+   --- --- ---
+C | O | O | X |
+   --- --- ---"""
+	
+	assert ( drawFilledTable(3, [["X", "O", " "], ["X", "X", "O"], ["O", "O", "X"]]) == filledTable) # done as 'approval test'
+	
 def drawEmptyRow(rowInfo, size):
 	return(rowInfo+" "+"|   "*size+"|")
 
@@ -40,8 +51,6 @@ def joinRowsWithNewLine(target):
 	return("\n".join(target))
 
 def drawTable(size):
-	# note - no tests for these
-	
 	rowLabels =    ["A", "B", "C", "D", "E", "F", "G", "H", "I"]
 	headerLabels = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 	collection = [makeHeaderRow(size, headerLabels)]
@@ -52,6 +61,18 @@ def drawTable(size):
 	
 	return(joinRowsWithNewLine(collection))
 
+def drawFilledTable(size, content):
+	# refactor later
+	rowLabels =    ["A", "B", "C", "D", "E", "F", "G", "H", "I"]
+	headerLabels = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+	collection = [makeHeaderRow(size, headerLabels)]
+	for i in range(size):
+		collection.append(drawDivider(size))
+		collection.append(drawFilledRow(rowLabels[i], size, content[i]))
+	collection.append(drawDivider(size))
+	return(joinRowsWithNewLine(collection))
+
 test() 
 
 print(drawTable(9))
+print(drawFilledTable(3, [["X", "O", " "], ["X", "X", "O"], ["O", "O", "X"]]))
