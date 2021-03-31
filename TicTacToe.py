e5c87b5640bf09a9e246954cf1401226f85ac712
James Lyndsay <jdl@workroom-productions.com>
2021-04-01 00:01:21 +0100

    Adding row labels - some pass, but fails not as label is not incremented

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,29 +1,30 @@
 #!/usr/bin/env python3
 
 def test():
-	assert( drawRow(3) == "|   |   |   |")
-	assert( drawDivider(3) == " --- --- ---")
-	table11 = """ ---
-|   |
- ---"""
-	table22 = """ --- ---
-|   |   |
- --- ---
-|   |   |
- --- ---"""
+	assert( drawRow("A",3) == "A |   |   |   |"), "produced "+drawRow("A",3) 
+	assert( drawDivider(3) == "   --- --- ---")
+	table11 = """   ---
+A |   |
+   ---"""
+	table22 = """   --- ---
+A |   |   |
+   --- ---
+B |   |   |
+   --- ---"""
 	assert( drawTable(1) == table11), "drawTable(1) produced "+drawTable(1)
 	assert( drawTable(2) == table22), "drawTable(2) produced "+drawTable(2)
 	
-def drawRow(size):
-	return("|   "*size+"|")
+def drawRow(rowInfo, size):
+	return(rowInfo+" "+"|   "*size+"|")
 
 def drawDivider(size):
-	return(" ---"*size)
+	return("  "+" ---"*size)
 
 def drawTable(size):
+	# note - no tests for these
 	def addTopAndCentre(target,numberOfColumns):
 		target.append(drawDivider(numberOfColumns))
-		target.append(drawRow(numberOfColumns))
+		target.append(drawRow("A",numberOfColumns))
 	
 	def addBottomRow(target, numberOfColumns):
 		target.append(drawDivider(numberOfColumns))
@@ -39,4 +40,5 @@ def drawTable(size):
 	return(joinRowsWithNewLine(collection))
 
 test() 
+
 print(drawTable(10))
