ffb2cd2f16c1f33853f4bf3b14859debd19df79c
James Lyndsay <jdl@workroom-productions.com>
2021-03-31 23:21:07 +0100

    asserton error on drawtable

--- TicTacToe.py
+++ TicTacToe.py
@@ -3,11 +3,17 @@
 def test():
 	assert( drawRow(3) == "|   |   |   |")
 	assert( drawDivider(3) == " --- --- ---")
+	assert( drawTable(1) == """ ---
+	|   |
+	 ---""")
 	
 def drawRow(size):
 	return("|   "*size+"|")
 
 def drawDivider(size):
 	return(" ---"*size)
+
+def drawTable(size):
+	return("")
 
 test() 
