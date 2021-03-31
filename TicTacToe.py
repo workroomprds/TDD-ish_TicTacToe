99a93cc740d8ee771fdea22a00ab5149b18cce18
James Lyndsay <jdl@workroom-productions.com>
2021-03-31 23:08:34 +0100

    First commit: assertion error

---
+++ TicTacToe.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+def test():
+	assert( drawRow(2) == "|   |   |")
+	assert( drawDivider(2) == " --- ---")
+	
+def drawRow(size):
+	return("")
+
+def drawDivider(size):
+	return("")
+
+test() 
