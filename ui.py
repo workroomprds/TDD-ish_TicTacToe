f43ff5442289b9576cf3a3cf5b1639bc2ce325fe
James Lyndsay <jdl@workroom-productions.com>
2021-04-07 17:23:02 +0100

    Added ui and utils from Rock Paper Scissors Put input and (most) output stuff into ui

---
+++ ui.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import utils
+
+def askForInput(request):
+	return(input(request))
+
+def output(stuff):
+	print(stuff)
+	
+def announceWinner(winner):
+	return(winner+" wins!" if (winner!="Draw") else "Draw")
+
+def build_input_request(player:str, desiredInputs:list)->str:
+	"""Returns string to ask player to input throw"""
+	return ("Put an "+player+" in space "+utils.return_list_of_values_separated_by_slashes(desiredInputs)+" : ")
+
+
+def request_valid_input(player:str, validSet:list, get_input:object)->str:
+	"""Returns a valid choice within the rules - choce from function (typically input)"""
+	#validSet = valid_input_for_game(rules)
+	request = build_input_request(player, validSet)
+	choice = ""
+	while not (utils.accept_input(validSet, choice) ):
+		choice = get_input(request)
+	return(choice)
+
+def test_Winner():
+	assert (announceWinner("Q") == "Q wins!")
+	assert (announceWinner("Draw") == "Draw")
+	
+
+test_Winner()
+	
